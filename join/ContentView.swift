e9e7c70b14f52fd7877c8dc50531047001dcdf10
Kushagra Sinha <kushagrasinha2002@gmail.com>
2023-12-31 20:48:07 +0530

    Changed Splash Screen Background Color

--- join/ContentView.swift
+++ join/ContentView.swift
@@ -20,9 +20,13 @@ struct ContentView: View {
             if showSplashScreen {
                 // showSplashScreen will change to false in 0.25 sec,
                 // which is handled in LottieView.swift
-                LottieView(showSplashScreen: $showSplashScreen,
-                           name: Constants.Lottie.launchScreen)
-                .scaleEffect(CGSize(width: 0.4, height: 0.4))
+                ZStack {
+                    Color.orange.opacity(0.1)
+                        .ignoresSafeArea()
+                    LottieView(showSplashScreen: $showSplashScreen,
+                               name: Constants.Lottie.launchScreen)
+                    .scaleEffect(CGSize(width: 0.4, height: 0.4))
+                }
             } else if closeLanguageSelectionScreen == false{
                 // Will appear with easeInOut(1.0 sec) animation
                 LanguageSelectionScreen(showOnBoardingScreen: $closeLanguageSelectionScreen)
